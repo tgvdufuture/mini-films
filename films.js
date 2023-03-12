33623ac50a73644a58f240358c99a7a7f31d9743
tgvdufuture <101932390+tgvdufuture@users.noreply.github.com>
2023-03-12 17:02:34 +0000

    names

--- films.js
+++ films.js
@@ -5,233 +5,227 @@ let filterarray =[];
 let galleryarray = [
     {
         id:1,
-        name : "l'ascension",
+        name : "L'Ascension",
         src : "assets/images/ascension.jpg",
         link : "./films/Ascension/index.html"
     },
     {
         id:2,
-        name : "avatar",
+        name : "Avatar",
         src : "assets/images/avatar.jpg",
         link : "./films/Avatar/index.html"
     },
     {
         id:3,
-        name : "avatar 2",
+        name : "Avatar 2",
         src : "assets/images/avatar2.jpg",
         link : "#"
     },
     {
         id:4,
-        name : "the batman",
+        name : "The Batman",
         src : "assets/images/batman.jpg",
         link : "#"
     },
     {
         id:5,
-        name : "captaine fantastique",
+        name : "Captaine Fantastique",
         src : "assets/images/cpt-fantastic.jpg",
         link : "#"
     },
     {
         id:6,
-        name : "ready player one",
+        name : "Ready Player One",
         src : "assets/images/ready-player-one.jpg",
         link : "#"
     },
     {
         id:7,
-        name : "retour vers le futur",
+        name : "Retour Vers Le Futur",
         src : "assets/images/retour1.jpg",
         link : "#"
     },
     {
         id:8,
-        name : "retour vers le futur 2",
+        name : "Retour Vers Le Futur 2",
         src : "assets/images/retour2.jpg",
         link : "#"
     },
     {
         id:9,
-        name : "retour vers le futur 3",
+        name : "Retour Vers Le Futur 3",
         src : "assets/images/retour3.jpg",
         link : "#"
     },
     {
         id:10,
-        name : "spiderman no way home",
+        name : "Spiderman No Way Home",
         src : "assets/images/spiderman.jpg",
         link : "#"
     },
     {
         id:11,
-        name : "star wras I : la menace fantôme",
+        name : "Star Wars I : la menace fantôme",
         src : "assets/images/star-wars1.jpg",
         link : "#"
     },
     {
         id:12,
-        name : "star wras II : l'attaque des clones",
+        name : "tar wars II : l'attaque des clones",
         src : "assets/images/star-wars2.jpg",
         link : "#"
     },
     {
         id:13,
-        name : "star wras III : la revanche des sith",
+        name : "Star Wars III : la revanche des sith",
         src : "assets/images/star-wars3.jpg",
         link : "#"
     },
     {
         id:14,
-        name : "star wras IV : un nouvel espoir",
+        name : "Star Wars IV : un nouvel espoir",
         src : "assets/images/star-wars4.jpg",
         link : "#"
     },
     {
         id:15,
-        name : "star wras V : l'empire contre-attaque",
+        name : "Star Wars V : l'empire contre-attaque",
         src : "assets/images/star-wars5.jpg",
         link : "#"
     },
     {
         id:16,
-        name : "star wras VI : le retour du Jedi",
+        name : "Star Wars VI : le retour du Jedi",
         src : "assets/images/star-wars6.jpg",
         link : "#"
     },
     {
         id:17,
-        name : "star wras VII : le réveil de la force",
+        name : "Star Wars VII : le réveil de la force",
         src : "assets/images/star-wars7.jpg",
         link : "#"
     },
     {
         id:18,
-        name : "star wras VIII : les derniers jedi",
+        name : "Star Wars VIII : les derniers jedi",
         src : "assets/images/star-wars8.jpg",
         link : "#"
     },
     {
         id:18,
-        name : "star wras IX : l'ascension de skywalker",
+        name : "Star Wars IX : l'ascension de skywalker",
         src : "assets/images/star-wars9.jpg",
         link : "#"
     },
     {
         id:19,
-        name : "black panther",
+        name : "Black Panther",
         src : "assets/images/black-panther.jpg",
         link : "#"
     },
     {
         id:20,
-        name : "black panther: wakanda forever",
+        name : "Black Panther: wakanda forever",
         src : "assets/images/black-panther2.jpg",
         link : "#"
     },
     {
         id:21,
-        name : "black adam",
+        name : "Black Adam",
         src : "assets/images/black-adam.jpg",
     link : "./films/BlackAdam/index.html"
     },
     {
         id:22,
-        name : "la grande aventure lego",
+        name : "La Grande Aventure Lego",
         src : "assets/images/lego.jpg",
         link : "#"
     },
     {
         id:23,
-        name : "fast and furious",
+        name : "Fast and Furious",
         src : "assets/images/fast and furious.jpg",
         link : "#"
     },
     {
         id:24,
-        name : "fast and furious 2",
+        name : "Fast and Furious 2",
         src : "assets/images/fast and furious 2.jpg",
         link : "#"
     },
     {
         id:25,
-        name : "fast and furious: tokyo drift",
+        name : "Fast and Furious: Tokyo Drift",
         src : "assets/images/fast and furious 3.jpg",
         link : "#"
     },
     {
         id:26,
-        name : "fast and furious 4",
+        name : "Fast and Furious 4",
         src : "assets/images/fast and furious 4.jpg",
         link : "#"
     },
     {
         id:27,
-        name : "fast and furious 5",
+        name : "Fast and Furious 5",
         src : "assets/images/fast and furious 5.jpg",
         link : "#"
     },
     {
         id:28,
-        name : "fast and furious 6",
+        name : "Fast and Furious 6",
         src : "assets/images/fast and furious 6.jpg",
         link : "#"
     },
     {
         id:29,
-        name : "fast and furious 7",
+        name : "Fast and Furious 7",
         src : "assets/images/fast and furious 7.jpg",
         link : "#"
     },
     {
         id:30,
-        name : "fast and furious 8",
+        name : "Fast and Furious 8",
         src : "assets/images/fast and furious 8.jpg",
         link : "#"
     },
     {
         id:31,
-        name : "fast and furious 9",
+        name : "Fast and Furious 9",
         src : "assets/images/fast and furious 9.jpg",
         link : "./films/FastAndFurious9/index.html"
     },
     {
         id:32,
-        name : "taxi",
+        name : "Taxi",
         src : "assets/images/taxi.jpg",
         link : "#"
     },
     {
         id:33,
-        name : "taxi 2",
+        name : "Taxi 2",
         src : "assets/images/taxi 2.jpg",
         link : "#"
     },
     {
         id:34,
-        name : "taxi 3",
+        name : "Taxi 3",
         src : "assets/images/taxi 3.jpg",
         link : "#"
     },
     {
         id:35,
-        name : "taxi 4",
+        name : "Taxi 4",
         src : "assets/images/taxi 4.jpg",
         link : "#"
     },
     {
         id:36,
-        name : "taxi 5",
+        name : "Taxi 5",
         src : "assets/images/taxi 5.jpg",
         link : "./films/Taxi5/index.html"
     },
-    {
-        id: 37,
-        name : "irréductibles",
-        src : "assets/images/irréductibles.jpg",
-        link : "#"
-    }
-   ];
+    ];
 
 
 showgallery(galleryarray);
